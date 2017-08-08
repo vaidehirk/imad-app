cf13382101d8a83fb0ba94bdd37bdac954198214
vaidehirk <30662346+vaidehirk@users.noreply.github.com>
2017-08-08 20:56:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/article-one', function (req, res) {
+  res.send('Article one is requested and served here.');
+});
+
+app.get('/article-two', function (req, res) {
+  res.send('Article two is requested and served here.');
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('Article three is requested and served here.');
 });
 
 app.get('/ui/madi.png', function (req, res) {
